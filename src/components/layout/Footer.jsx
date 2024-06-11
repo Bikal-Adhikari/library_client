d5ecf9732666856b93b4a45bd6ba0c32ac519ad0
Bikal <82023507+Bikal-Adhikari@users.noreply.github.com>
2024-06-11 13:35:51 +1000

    Vercel config

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -4,7 +4,7 @@ export const Footer = () => {
   return (
     <div className="bg-dark text-light p-5 text-center  ">
       {" "}
-      &copy; Copy right all reserved. Made by <a href="">Prem</a> with fun!{" "}
+      &copy; Copy right all reserved. Made by <a href="">Bikal</a> with fun!{" "}
     </div>
   );
 };
