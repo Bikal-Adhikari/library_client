216223a740c8426ba7ca7079e3f1dc73ced66d33
Bikal <82023507+Bikal-Adhikari@users.noreply.github.com>
2024-06-15 22:32:08 +1000

    Inputs field are created

--- src/pages/user/EditProfile.jsx
+++ src/pages/user/EditProfile.jsx
@@ -6,17 +6,49 @@ import useForm from "../../hooks/useForm";
 
 const EditProfile = () => {
   const dispatch = useDispatch();
-  const { form, handleOnChange, setForm } = useForm({});
+
   const { user } = useSelector((state) => state.userInfo);
+  const { form, handleOnChange, setForm } = useForm({ user });
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
-    const { __v, createdAt, isbn, updatedAt, ...rest } = form;
+    const { __v, createdAt, updatedAt, ...rest } = form;
 
     if (window.confirm("Are you sure you want to make this changes?")) {
       dispatch(updateSingleBookAction(rest));
     }
   };
+
+  const inputs = [
+    {
+      label: "First Name",
+      name: "fName",
+      type: "text",
+      required: true,
+      placeholder: "Write your first name",
+    },
+    {
+      label: "Last Name",
+      name: "lName",
+      type: "text",
+      required: true,
+      placeholder: "Write your Last name",
+    },
+    {
+      label: "Phone",
+      name: "phone",
+      type: "number",
+      required: false,
+      placeholder: "041345678",
+    },
+    {
+      label: "Email",
+      name: "email",
+      type: "email",
+      required: true,
+      placeholder: "email@email.com",
+    },
+  ];
 
   return (
     <UserLayout pageTitle={"Update Profile"}>
@@ -26,7 +58,7 @@ const EditProfile = () => {
         <h4 className="py-4">Update the new book</h4>
 
         <Form onSubmit={handleOnSubmit}>
-          {inputFields?.map((input, i) => (
+          {inputs?.map((input, i) => (
             <CustomInput
               disabled={input.name === "isbn"}
               key={i}
